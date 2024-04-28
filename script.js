c37323c581278b059cf67e387a29d348eebfa18c
bpiont <97611567+bpiont@users.noreply.github.com>
2024-04-28 19:14:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,23 @@
-// Funkcja pobierająca dane o bocie z Discord API
-function fetchBotData() {
+// Funkcja pobierająca i aktualizująca dane o bocie z Discord API
+function fetchAndUpdateBotData() {
   fetch('https://discord.com/api/bots/BOT_ID', {
     headers: {
-      'Authorization': 'MTIzMzkxNjIwMjQzNTM0NjQzMg.G21EGf.X6jycfCjAcJZ5d0nE17wqFdYmX4yrDg5ggPmc4'
+      'Authorization': 'MTIzMzkxNjIwMjQzNTM0NjQzMg.Gr0DQI.f24JTowvE1NTjhdeO-P3VofiAoqNQMZ7WkEHyw'
     }
   })
   .then(response => response.json())
   .then(data => {
-    // Tutaj przetwarzaj dane i aktualizuj zawartość strony
-    updatePageWithBotData(data);
+    // Aktualizuj dane o bocie na stronie
+    document.getElementById('botUsersCount').textContent = data.approximate_guild_count;
+    document.getElementById('botStatus').textContent = data.status;
   })
   .catch(error => console.error('Błąd pobierania danych o bocie:', error));
 }
 
-// Funkcja aktualizująca zawartość strony z danymi o bocie
-function updatePageWithBotData(botData) {
-  document.getElementById('botUsersCount').textContent = botData.usersCount;
-  document.getElementById('botStatus').textContent = botData.status;
-}
-
-// Wywołanie funkcji pobierającej dane po załadowaniu strony
+// Wywołaj funkcję fetchAndUpdateBotData() po załadowaniu strony
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBotData();
-  // Możesz również dodać setInterval(fetchBotData, 60000); aby regularnie odświeżać dane co minutę
+  fetchAndUpdateBotData();
+  // Uruchom funkcję fetchAndUpdateBotData() co minutę
+  setInterval(fetchAndUpdateBotData, 60000);
 });
+
