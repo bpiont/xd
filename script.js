c4eb81f533cadb58cc35ce442e07bf3dfa82bf58
bpiont <97611567+bpiont@users.noreply.github.com>
2024-04-28 19:19:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,13 @@
-// Funkcja pobierająca i aktualizująca dane o bocie z Discord API
-function fetchAndUpdateBotData() {
-  fetch('https://discord.com/api/v9/guilds/GUILD_ID/widget.json', {
-    headers: {
-      'Authorization': 'MTIzMzkxNjIwMjQzNTM0NjQzMg.Gr0DQI.f24JTowvE1NTjhdeO-P3VofiAoqNQMZ7WkEHyw'
-    }
-  })
-  .then(response => response.json())
-  .then(data => {
-    // Aktualizuj dane o bocie na stronie
-    document.getElementById('botUsersCount').textContent = data.approximate_guild_count;
-    document.getElementById('botStatus').textContent = data.status;
-  })
-  .catch(error => console.error('Błąd pobierania danych o bocie:', error));
-}
-
-// Wywołaj funkcję fetchAndUpdateBotData() po załadowaniu strony
-document.addEventListener('DOMContentLoaded', function() {
-  fetchAndUpdateBotData();
-  // Uruchom funkcję fetchAndUpdateBotData() co minutę
-  setInterval(fetchAndUpdateBotData, 60000);
-});
-
+fetch('https://discord.com/api/v9/guilds/GUILD_ID/widget.json', {
+  headers: {
+    'Authorization': 'MTIzMzkxNjIwMjQzNTM0NjQzMg.Gr0DQI.f24JTowvE1NTjhdeO-P3VofiAoqNQMZ7WkEHyw'
+  }
+})
+.then(response => response.json())
+.then(data => {
+  const botStatus = data.instant_invite ? 'Online' : 'Offline';
+  const botUsersCount = data.members.length;
+  document.getElementById('botStatus').textContent = botStatus;
+  document.getElementById('botUsersCount').textContent = botUsersCount;
+})
+.catch(error => console.error('Błąd pobierania danych o bocie:', error));
