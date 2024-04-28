957ef0087f8d1f0895496062d758f6c7dd3304b2
bpiont <97611567+bpiont@users.noreply.github.com>
2024-04-28 19:21:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-fetch('https://discord.com/api/v9/guilds/GUILD_ID/widget.json', {
-  headers: {
-    'Authorization': 'MTIzMzkxNjIwMjQzNTM0NjQzMg.Gr0DQI.f24JTowvE1NTjhdeO-P3VofiAoqNQMZ7WkEHyw'
-  }
-})
-.then(response => response.json())
-.then(data => {
-  const botStatus = data.instant_invite ? 'Online' : 'Offline';
-  const botUsersCount = data.members.length;
-  document.getElementById('botStatus').textContent = botStatus;
-  document.getElementById('botUsersCount').textContent = botUsersCount;
-})
-.catch(error => console.error('Błąd pobierania danych o bocie:', error));
+
